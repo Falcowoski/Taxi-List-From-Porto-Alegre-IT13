4d5c5d1649d16d97123e319cbe00cecfd02f51d3
Leonardo Falcoski <leonardo.falcoski@gmail.com>
2021-07-27 01:52:57 -0300

    Novas funções; Novo Módulo; Mudança no visual

--- main.js
+++ main.js
@@ -3,14 +3,16 @@
 // Prompt Module
 const prompt = require('prompt-sync')()
 
+// Colors Module
+const colors = require('colors')
+
 // Data Module
 const data = require('./data.js').arr
 
 // Taxi Points List
 var indexAux = 0
 
-function list(array, start, end) { // data, 0, 9
-
+function list(array, start, end, distanceArraySorted) {
     for (let index = start; index <= end; index++) {
         if (array[index] == null) {
             end = 0
@@ -23,16 +25,27 @@ function list(array, start, end) { // data, 0, 9
             let tel = array[index]['telefone']
             let code = array[index]['code']
     
-            console.log(`============ PONTO ${index + 1} ============`)
-            console.log(`NOME DO PONTO: ${nome} \nENDEREÇO: ${[log]} \nNÚMERO: ${number} \nTELEFONE: ${tel} \nCÓDIGO: ${code}\n`)
+            console.log(`============ PONTO ${index + 1} ==================================`.bold .bgGreen)
+            console.log(`NOME DO PONTO: ${nome} \nENDEREÇO: ${[log]} \nNÚMERO: ${number} \nTELEFONE: ${tel} \nCÓDIGO: ${code}`)
+
+            if (distanceArraySorted == undefined) {
+                console.log(`CÓDIGO: ${code}\n`)
+            } else console.log(`CÓDIGO: ${code}`)
+
+            if (distanceArraySorted != undefined) {
+                let distance = distanceArraySorted[index]['distance']
+                if (distance >= 1) {
+                    console.log(`DISTÂNCIA: ${distance.toFixed(1)} QUILÔMETROS\n`)
+                } else console.log(`DISTÂNCIA: ${(distance * 1000).toFixed()} METROS\n`)
+            }
         }
     }
 
     let avaliablePoints = (array.length - 1) - indexAux
 
     if (avaliablePoints >= 1) { 
-        console.log(`Ainda há ${avaliablePoints} pontos disponíveis. O quê você deseja fazer?\n`)
-        console.log("1 - Pular 1 página\n2 - Pular 5 páginas\n3 - Pular 10 páginas\n4 - Sair\n")
+        console.log(`Ainda há ${avaliablePoints} pontos disponíveis. O quê você deseja fazer?\n`.green)
+        console.log("1 - Pular 1 página\n2 - Pular 5 páginas\n3 - Pular 10 páginas\n9 - Sair\n")
     
         let option = Number(prompt("Opção: "))
     
@@ -49,8 +62,8 @@ function list(array, start, end) { // data, 0, 9
                 //console.log("indexAux:", indexAux) //
                 list(data, indexAux + 1, indexAux + 100)
                 break;
-            case 4:
-                message("Retornando ao menu principal...", 1000)
+            case 9:
+                message("Retornando ao menu principal...", 500)
                 break;
             default:
                 message("Ops! Não foi possível entender o seu pedido. Tente novamente!", 1000) 
@@ -58,6 +71,26 @@ function list(array, start, end) { // data, 0, 9
         }
     } else wait()
 
+}
+
+function searchIndexArrayAndList(array, distanceArray, distanceArraySorted) {
+    let arrayWithDistance = []
+    let arrayWithDistanceAux = []
+    let pointDistance = 0
+
+    function indexLoop (element, index) {
+        element[index] = element.distance = pointDistance
+    }
+
+    for (let index = 0; index <= 2; index++) {
+        pointDistance = distanceArraySorted[index]
+        let pointIndex = distanceArray.findIndex(distance => distance == pointDistance)
+        arrayWithDistanceAux.push((array[pointIndex]))
+        arrayWithDistance = JSON.parse(JSON.stringify(arrayWithDistanceAux))
+    }
+
+    arrayWithDistance.forEach((element, index) => element.distance = distanceArraySorted[index])
+    list(arrayWithDistance, 0, 2, arrayWithDistance)
 }
 
 // Haversine Formula
@@ -76,10 +109,40 @@ function haversine() {
     return d
 }
 
+// Calculate distance (Origin -> Taxi Points) with Haversine Formula
+var latAux = undefined
+var longAux = undefined
+
+function calculeDistanceArray(array, latitude, longitude) {
+
+    if (latitude == latAux && longitude == longAux) {
+        // exiba novamente a lista
+
+    } else {
+        let distanceArray = []
+        let distanceArraySorted = []
+        
+        for(let index = 0; index < array.length; index++) {
+            //let latI = Number(searchValueArray(array, index, 'latitude'))
+            //let longI = Number(searchValueArray(array, index, 'longitude'))
+            let latI = Number( (array[index]["latitude"]) )
+            let longI = Number( (array[index]["longitude"]) )
+            let hav = haversine(lat, long, latI, longI)
+            distanceArray.push(hav)
+        }
+
+        return {
+            distanceArray: distanceArray,
+            distanceArraySorted: distanceArraySorted = distanceArray.slice().sort()
+        }
+    }
+}
+
+
 // Menu Functions
 function message(msg, timems) {
     var time = timems == undefined ? 2000 : timems
-    console.log("\nSistema:", msg)
+    console.log("\nSistema:".italic .green, msg)
     sleep(time)
 }
 
@@ -92,31 +155,35 @@ function sleep(ms) {
 }
 
 function wait() {
-    let p = prompt("Pressione Enter para retornar...")
+    let p = prompt("Pressione Enter para retornar...".italic .green)
 }
 
 //Menu 
 do {
     console.clear()
-    console.log("=============== MENU ===============\n")
+    console.log(`=============== ${"MENU".white}`.green, "=================\n".green)
     console.log("1 - Listar todos os pontos de taxi") 
     console.log("2 - Informar minha localização")
     console.log("3 - Encontrar pontos próximos")
     console.log("4 - Buscar pontos por logradouro")
     console.log("9 - Sair\n")
-    console.log("====================================\n")
+    console.log(`================ ${"LF".white}`.green, "==================\n".green)
     var option = Number(prompt("Opção: "))
 
     switch(option) {
         case 0: // test mode
             lat = -30.04380072
             long = -51.22332745
+            console.log(`\n=========== ${"Coordenadas definidas!".white}`.green, "==============\n".green)
+            console.log("Latitude:", lat, "| Longitude:", long)
+            console.log(`\n=========== ${"Coordenadas definidas!".white}`.green, "==============".green)
+            message("Localização armazenada!", 800)
             break
         case 1:
             list(data, 0, 9)
             break
         case 2:
-            console.log("Exemplo: -5.002544875")
+            console.log("Exemplo: -5.002544875".italic .green)
             lat = Number(prompt("Digite sua latitude em formato geodésico decimal: "))
             long = Number(prompt("Digite sua longitutde em formato geodésico decimal: "))
             if (isNaN(lat && long) != true ) {
@@ -128,7 +195,13 @@ do {
             }
             break    
         case 3:
-
+            if (lat && long != undefined) {
+                console.log("\nOs três pontos mais próximos são:\n".bold .green)
+                let distanceArrayAux = calculeDistanceArray(data, lat, long)
+                let distanceArray = distanceArrayAux.distanceArray
+                let distanceArraySorted = distanceArrayAux.distanceArraySorted
+                searchIndexArrayAndList(data, distanceArray, distanceArraySorted)
+            } else message("Ops! A localização do usuário é desconhecida")
             break
         case 4:
             
